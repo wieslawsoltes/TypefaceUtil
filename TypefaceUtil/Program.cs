bd63101c0c411c4d25a5a5200c4c32acb50c6678
wieslawsoltes <wieslaw.soltes@gmail.com>
2020-07-01 11:55:36 +0000

    Added xaml support

--- TypefaceUtil/Program.cs
+++ TypefaceUtil/Program.cs
@@ -540,13 +540,53 @@ namespace TypefaceUtil
 
                 using var outlinePath = skTextPaint.GetTextPath(utf32, x, y);
                 using var fillPath = skTextPaint.GetFillPath(outlinePath);
+
+                fillPath.Transform(SKMatrix.MakeTranslation(-fillPath.Bounds.Left, -fillPath.Bounds.Top));
                 var bounds = fillPath.Bounds;
+
                 var svgPathData = fillPath.ToSvgPathData();
 
                 streamWriter.WriteLine($"[{utf32}]");
                 streamWriter.WriteLine($"<svg viewBox=\"{bounds.Left} {bounds.Top} {bounds.Width} {bounds.Height}\" xmlns=\"http://www.w3.org/2000/svg\">"); // width=\"{bounds.Width}\" height=\"{bounds.Height}\"
                 streamWriter.WriteLine($"  <path fill=\"{fill}\" d=\"{svgPathData}\"/>");
                 streamWriter.WriteLine($"</svg>");
+            }
+        }
+
+        static void SaveCharacterMapAsXaml(Dictionary<int, ushort> characterToGlyphMap, SKTypeface typeface, string path, float textSize, string brush)
+        {
+            using var skTextPaint = new SKPaint();
+            skTextPaint.IsAntialias = true;
+            skTextPaint.Color = new SKColor(0x00, 0x00, 0x00);
+            skTextPaint.Typeface  = typeface;
+            skTextPaint.TextEncoding  = SKTextEncoding.Utf32;
+            skTextPaint.TextSize = textSize;
+            skTextPaint.TextAlign = SKTextAlign.Center;
+            skTextPaint.LcdRenderText = true;
+            skTextPaint.SubpixelText = true;
+
+            var metrics = skTextPaint.FontMetrics;
+            var mAscent = metrics.Ascent;
+            var mDescent = metrics.Descent;
+
+            using var streamWriter = File.CreateText(path);
+
+            foreach (var kvp in characterToGlyphMap)
+            {
+                var charCode = kvp.Key;
+                var utf32 = Char.ConvertFromUtf32((int)charCode);
+                float x = 0;
+                float y = (mAscent / 2.0f) - mDescent / 2.0f;
+
+                using var outlinePath = skTextPaint.GetTextPath(utf32, x, y);
+                using var fillPath = skTextPaint.GetFillPath(outlinePath);
+
+                fillPath.Transform(SKMatrix.MakeTranslation(-fillPath.Bounds.Left, -fillPath.Bounds.Top));
+
+                var svgPathData = fillPath.ToSvgPathData();
+
+                streamWriter.WriteLine($"[{utf32}]");
+                streamWriter.WriteLine($"<GeometryDrawing Brush=\"{brush}\" Geometry=\"{svgPathData}\"/>"); //  x:Key=\"{key}\"
             }
         }
 
@@ -564,7 +604,8 @@ namespace TypefaceUtil
                         foreach (var characterMap in characterMaps)
                         {
                             SaveCharacterMapAsPng(characterMap.CharacterToGlyphMap, typeface, $"charmap_({typeface.FamilyName})_{characterMap.Name}.png");
-                            SaveCharacterMapAsSvg(characterMap.CharacterToGlyphMap, typeface, $"charmap_({typeface.FamilyName})_{characterMap.Name}.txt", 20f, "black");
+                            SaveCharacterMapAsSvg(characterMap.CharacterToGlyphMap, typeface, $"charmap_({typeface.FamilyName})_{characterMap.Name}.svg.txt", 16f, "black");
+                            SaveCharacterMapAsXaml(characterMap.CharacterToGlyphMap, typeface, $"charmap_({typeface.FamilyName})_{characterMap.Name}.xaml.txt", 16f, "Black");
                         }
                     }
                 }
@@ -578,7 +619,8 @@ namespace TypefaceUtil
                         foreach (var characterMap in characterMaps)
                         {
                             SaveCharacterMapAsPng(characterMap.CharacterToGlyphMap, typeface, $"charmap_({typeface.FamilyName})_{characterMap.Name}.png");
-                            SaveCharacterMapAsSvg(characterMap.CharacterToGlyphMap, typeface, $"charmap_({typeface.FamilyName})_{characterMap.Name}.txt", 20f, "black");
+                            SaveCharacterMapAsSvg(characterMap.CharacterToGlyphMap, typeface, $"charmap_({typeface.FamilyName})_{characterMap.Name}.svg.txt", 16f, "black");
+                            SaveCharacterMapAsXaml(characterMap.CharacterToGlyphMap, typeface, $"charmap_({typeface.FamilyName})_{characterMap.Name}.xaml.txt", 16f, "Black");
                         }
                     }
                 }
