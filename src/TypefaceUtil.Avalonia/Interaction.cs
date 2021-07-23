678bd884df4cab15c1a521daf2a4adc7c62356b3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-23 23:07:20 +0200

    Create Interaction.cs

---
+++ src/TypefaceUtil.Avalonia/Interaction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace TypefaceUtil.Avalonia
+{
+    public abstract class Behavior
+    {
+        
+    }
+    
+    public class Interaction
+    {
+        static Interaction()
+        {
+            var onClickHandlers = new Dictionary<Button, EventHandler<RoutedEventArgs>>();
+
+            OnClickProperty.Changed.Subscribe(e =>
+            {
+                var oldAction = e.OldValue.GetValueOrDefault();
+                var newAction = e.NewValue.GetValueOrDefault();
+
+                if (oldAction == newAction)
+                {
+                    return;
+                }
+
+                if (e.Sender is Button button)
+                {
+                    if (oldAction is { } && onClickHandlers.ContainsKey(button))
+                    {
+                        EventHandler<RoutedEventArgs> handler = onClickHandlers[button];
+                        button.Click -= handler;
+                    }
+
+                    if (newAction is { })
+                    {
+                        EventHandler<RoutedEventArgs> handler = (_, _) => newAction?.Invoke();
+                        button.Click += handler;
+                        onClickHandlers.Add(button, handler);
+                    }
+                }
+            });
+        }
+        
+        public static readonly AttachedProperty<Action> OnClickProperty = 
+            AvaloniaProperty.RegisterAttached<Interaction, Button, Action>("OnClick");
+
+        public static Action GetOnClick(Button button)
+        {
+            var action = button.GetValue(OnClickProperty);
+
+            button.Click += (_, _) => action?.Invoke();
+
+            return action;
+        }
+
+        public static void SetOnClick(Button button, Action action)
+        {
+            button.SetValue(OnClickProperty, action);
+            
+            
+            button.Click += (_, _) => action?.Invoke();
+
+        }
+    }
+}
