cbda81fab7fa1cbbd505ca6f2b1854e83b54d28e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-25 21:48:30 +0200

    Move

--- src/TypefaceUtil.Avalonia/Views/MainWindow.axaml.cs
+++ src/TypefaceUtil.Avalonia/Views/MainWindow.axaml.cs
@@ -1,11 +1,18 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace TypefaceUtil.Avalonia.Views
 {
     public partial class MainWindow : Window
     {
+        private bool _isDragging = false;
+        private Control? _dragControl;
+        private Point _startPoint;
+        private TranslateTransform? _dragTransform;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -17,6 +24,41 @@ namespace TypefaceUtil.Avalonia.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void GlyphView_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            _dragControl = sender as Control;
+            if (_dragControl is { })
+            {
+                if (_dragTransform is null)
+                {
+                    _dragTransform = new TranslateTransform();
+                    _dragControl.RenderTransform = _dragTransform;
+                }
+                _startPoint = e.GetPosition(this) - new Point(_dragTransform.X, _dragTransform.Y);
+                _isDragging = true;
+            }
+        }
+
+        private void GlyphView_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+        {
+            if (_isDragging && _dragControl is { })
+            {
+                _isDragging = false;
+                _dragControl = null;
+            }
+        }
+
+        private void GlyphView_OnPointerMoved(object? sender, PointerEventArgs e)
+        {
+            if (_isDragging && _dragControl is { } && _dragTransform is { })
+            {
+                var currentPoint = e.GetPosition(this);
+                var delta = currentPoint - _startPoint;
+                _dragTransform.X = delta.X;
+                _dragTransform.Y = delta.Y;
+            }
         }
     }
 }
