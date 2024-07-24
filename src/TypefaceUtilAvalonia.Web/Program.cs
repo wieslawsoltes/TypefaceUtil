a51a2d3268ff41afc5f01f6b229fed9f068cd801
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-07-24 23:33:17 +0200

    Update Program.cs

--- src/TypefaceUtilAvalonia.Web/Program.cs
+++ src/TypefaceUtilAvalonia.Web/Program.cs
@@ -1,14 +1,17 @@
 using System.Runtime.Versioning;
+using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Browser;
 using TypefaceUtil.Avalonia;
 
 [assembly:SupportedOSPlatform("browser")]
 
 internal class Program
 {
-    private static void Main(string[] args) 
-        => BuildAvaloniaApp().SetupBrowserApp("out");
+    private static async Task Main(string[] args) 
+        => await BuildAvaloniaApp().StartBrowserAppAsync("out");
 
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>();
+        => AppBuilder
+            .Configure<App>();
 }
